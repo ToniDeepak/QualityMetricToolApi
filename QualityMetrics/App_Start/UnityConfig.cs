8f4e2554dbe9a7c38522801afc273c5e05b8ae1a
ToniDeepak <urssethi.deepak@gmail.com>
2021-08-14 13:27:20 +0530

    removed commented codes

--- QualityMetrics/App_Start/UnityConfig.cs
+++ QualityMetrics/App_Start/UnityConfig.cs
@@ -14,25 +14,10 @@ namespace QualityMetrics
         {
             var container = new UnityContainer();
 
-            //System.Web.Mvc.DependencyResolver.SetResolver(new UnityDependencyResolver(container));
-            //GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
             GlobalConfiguration.Configuration.DependencyResolver = new Unity.WebApi.UnityDependencyResolver(container);
             container.RegisterType<IExcelService, ExcelService>();
             container.RegisterType<IPeerCodeReviewService, PeerCodeReviewService>();
             container.RegisterType<ITeamFoundationService, TeamFoundationService>();
-            // register all your components with the container here
-            // it is NOT necessary to register your controllers
-
-            // e.g. container.RegisterType<ITestService, TestService>();
-            //RegisterTypes(container);
         }
-
-
-        //public static void RegisterTypes(IUnityContainer container)
-        //{
-        //    ComponentLoader.LoadContainer(container, ".\\bin", "TheWork.dll");
-        //    ComponentLoader.LoadContainer(container, ".\\bin", "BusinessServices.dll");
-        //    ComponentLoader.LoadContainer(container, ".\\bin", "DataModel.dll");
-        //}
     }
 }
